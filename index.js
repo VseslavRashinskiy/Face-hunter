117045f5e19dd06b4fced8ed350a4c9a0d3bc6bd
Vseslav Rashchynski <vrashinskiyv@gmail.com>
2023-10-18 16:22:20 +0200

    test

--- index.js
+++
@@ -1,9 +0,0 @@
-const video = document.getElementById('video');
-
-navigator.mediaDevices.getUserMedia({ video: true, audio: false })
-    .then((stream)=> {
-        video.srcObject = stream;
-    })
-    .catch( (err)=> {
-        console.error('Error accessing the camera: ', err);
-    });
